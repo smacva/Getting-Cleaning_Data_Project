ac65984f87567d3bb5227cc08ee2b49a38bf90b9
Scott MacDonald <smacdona@aer.com>
2016-02-27 08:23:18 -0500

    Update runAnalysis.r

--- runAnalysis.r
+++ runAnalysis.r
@@ -9,8 +9,8 @@
 # 2. Extract only the measurements on the mean and standard deviation for each measurement. 
 # 3. Use descriptive activity names to name the activities in the data set
 # 4. Appropriately label the data set with descriptive activity names. 
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and  
-each subject. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and
+     each subject. 
 ##
 
 # Clean up workspace
